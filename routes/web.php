cbdfb183a6768be7109c15a13c4437a1ee52deac
Chen Ching Ling <3a832046@gm.student.ncut.edu.tw>
2022-12-11 16:49:41 +0800

    設定route及對應的Controller&設定Controller回傳對應的view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('post');
+    return redirect(route('posts.index'));
 });
+
+Route::get('posts',[PostController::class, 'index'])->name('posts.index');
+Route::get('post',[PostController::class, 'show'])->name('posts.show');
+Route::get('contact',[PostController::class, 'contact'])->name('posts.contact');
+Route::get('about',[PostController::class, 'about'])->name('posts.about');
